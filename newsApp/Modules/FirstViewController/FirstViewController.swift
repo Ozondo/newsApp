d36d4d95efd6972fcfbd9169371394d9d56888b1
egorivanov <joker1catpro@gmail.com>
2023-09-12 18:26:42 +0300

    begin of project

--- newsApp/Modules/FirstViewController/FirstViewController.swift
+++ newsApp/Modules/FirstViewController/FirstViewController.swift
@@ -7,9 +7,33 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+final class FirstViewController: UIViewController {
+    private lazy var newsTableView = UITableView(frame: CGRect.zero, style: .plain)
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
+        viewSetup()
+        elementsSetup()
+        constrainSetup()
+    }
+}
+
+private extension FirstViewController{
+    func viewSetup(){
+        view.backgroundColor = .white
+        view.addSubview(newsTableView)
+    }
+    func elementsSetup(){
+        newsTableView.backgroundColor = .black
+    }
+    func constrainSetup(){
+        newsTableView.translatesAutoresizingMaskIntoConstraints = false
+
+        newsTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        newsTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+
+        newsTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10).isActive = true
+
+        newsTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
     }
 }
